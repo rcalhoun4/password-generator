9266e77c24ed409ff17985a83c9f9a5c926cd612
unknown <ryan.calhoun33@gmail.com>
2022-01-16 21:25:05 -0500

    finished password generator

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
-// Get references to the #generate element
+// Creates a variable for the button in the html
 const btn = document.getElementById('generate');
-const passwordDisplay = getElementById('password');
 
+// Creates a variable for the password display array in the html
+const passwordDisplay = document.getElementById('password');
+
+// Declares variables for each parameter as the arrays for each set of ascii characters (see function on line 56)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
@@ -21,26 +24,35 @@ btn.addEventListener('click', e => {
     const passwordSymbol = window.confirm('Would you like to include special characters in your password?');
     console.log(passwordSymbol);
 
-    //const password = generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol);
-    //passwordDisplay.innerText = password;
+    // Declares the password variable as the generatePassword function and passes in the parameters from above
+    const password = generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol);
+
+    // Displays the password on the page
+    passwordDisplay.innerText = password;
 });
 
 // This function creates the password based off of the parameters
 function generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol) {
+    // Makes lowercase letters the default for the password
     let charCodes = LOWERCASE_CHAR_CODES;
+
+    // Adds or omits uppercase, numbers, and symbols respectively based on their boolean value
     if (passwordCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
     if (passwordNumb) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
     if (passwordSymbol) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
 
+    // Chooses the characters from above at random pushes them to the empty array passwordCharacters  
     const passwordCharacters = [];
     for (let i = 0; i < passwordLength; i++) {
-        const characterCode = charCodes[Math.floor(Math.random() * passwordLength)];
+        const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)];
         passwordCharacters.push(String.fromCharCode(characterCode));
     }
 
+    // Turns passwordCharacters into a string
     return passwordCharacters.join('');
 }
 
+// Loops through and creates an array for the ascii character codes
 function arrayFromLowToHigh(low, high) {
     const array = [];
     for (let i = low; i <= high; i++) {
