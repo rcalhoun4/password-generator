ccd6efd60b19b200093857d762c3aecb1e7043ea
unknown <ryan.calhoun33@gmail.com>
2022-01-16 20:40:35 -0500

    second commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,51 @@
-// Assignment code here
-
-
 // Get references to the #generate element
-var btn = document.getElementById('#generate');
+const btn = document.getElementById('generate');
+const passwordDisplay = getElementById('password');
 
-// Add event listener to generate button
-btn.addEventListener('click', writePassword);
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57);
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
 
-// Write password to the #password input
-function writePassword(e) {
-  var passwordLength = window.prompt('How long would you like your password to be? Choose a number between 8 and 128.');
-  console.log(passwordLength);
+// This event listener creates the parameter prompts for the password
+btn.addEventListener('click', e => {
+    const passwordLength = window.prompt('How long would you like your password to be? Choose a number between 8 and 128.');
+    console.log(passwordLength);
 
-  var passwordCase = window.prompt('Would you like to include uppercase letters, lowercase letters, or both? Type UPPERCASE, LOWERCASE, or BOTH to choose.');
-  console.log(passwordCase);
+    const passwordCase = window.confirm('Would you like to include uppercase letters?');
+    console.log(passwordCase);
 
-  var passwordNumb = window.confirm('Would you like to include numbers in your password?');
-  console.log(passwordNumb);
+    const passwordNumb = window.confirm('Would you like to include numbers in your password?');
+    console.log(passwordNumb);
 
-  var passwordSymbol = window.confirm('Would you like to include special characters in your password?');
-  console.log(passwordSymbol);
+    const passwordSymbol = window.confirm('Would you like to include special characters in your password?');
+    console.log(passwordSymbol);
 
-  var password = generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol);
-  
-  return;
+    //const password = generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol);
+    //passwordDisplay.innerText = password;
+});
 
+// This function creates the password based off of the parameters
+function generatePassword(passwordLength, passwordCase, passwordNumb, passwordSymbol) {
+    let charCodes = LOWERCASE_CHAR_CODES;
+    if (passwordCase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES);
+    if (passwordNumb) charCodes = charCodes.concat(NUMBER_CHAR_CODES);
+    if (passwordSymbol) charCodes = charCodes.concat(SYMBOL_CHAR_CODES);
+
+    const passwordCharacters = [];
+    for (let i = 0; i < passwordLength; i++) {
+        const characterCode = charCodes[Math.floor(Math.random() * passwordLength)];
+        passwordCharacters.push(String.fromCharCode(characterCode));
+    }
+
+    return passwordCharacters.join('');
+}
+
+function arrayFromLowToHigh(low, high) {
+    const array = [];
+    for (let i = low; i <= high; i++) {
+        array.push(i);
+    }
+
+    return array;
 }
